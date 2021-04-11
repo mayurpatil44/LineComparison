96b83d2e6f75e8fb618de704f6ed3829035e27de
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-11 22:48:17 +0530

    added OOPS in program

--- LineComparison.java
+++ LineComparison.java
@@ -2,28 +2,49 @@ import java.lang.Math;
 import java.util.*;
 public class LineComaprison
 {
+	private double LengthOfLine1;
+	private double LengthOfLine2;
 
-	public static void main(String[] args)
+	private int x1;
+	private int x2;
+	private int x3;
+	private int x4;
+
+	private int y1;
+	private int y2;
+	private int y3;
+	private int y4;
+
+	public void readCoordinates()
 	{
+		Scanner sc1 = new Scanner(System.in);
 
-		int x1=2;
-		int x2=3;
-		int y1=2;
-		int y2=4;
+		System.out.println("Read X coordinates");
+		x1 = sc1.nextInt();
+		x2 = sc1.nextInt();
+		x3 = sc1.nextInt();
+		x4 = sc1.nextInt();
+		System.out.println("Read Y coordinates");
+		y1 = sc1.nextInt();
+		y2 = sc1.nextInt();
+		y3 = sc1.nextInt();
+		y4 = sc1.nextInt();
 
-		int x3=2;
-		int x4=5;
-		int y3=2;
-		int y4=4;
+	}
 
-		double LengthOfLine1 = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+	public void calculateLenght()
+	{
+		LengthOfLine1 = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
 		System.out.println("Length of line 1 is: " +LengthOfLine1);
 
-		double LengthOfLine2 = Math.sqrt((x4-x3)^2 + (y4-y3)^2);
+		LengthOfLine2 = Math.sqrt((x4-x3)^2 + (y4-y3)^2);
 		System.out.println("Length of line 2 is: " +LengthOfLine2);
 
-		double obj1 = new Double(LengthOfLine1);
-		double obj2 = new Double(LengthOfLine2);
+	}
+	public void compareTwoLine()
+	{
+		Double obj1 = new Double(LengthOfLine1);
+		Double obj2 = new Double(LengthOfLine2);
 
 		int compare = obj1.compareTo(obj2);
 
@@ -32,6 +53,16 @@ public class LineComaprison
 		else if(compare == -1)
 			System.out.println("Line 1 is less than Line 2");
 		else if(compare == 0)
-			System.out.println("Line1 and Line2 are equals");
+		System.out.println("Line1 and Line2 are equals");
+	}
+	public static void main(String[] args)
+	{
+
+		LineComaprison L1 = new LineComaprison();
+		L1.readCoordinates();
+		L1.calculateLenght();
+		L1.compareTwoLine();
+
 	}
 }
+
