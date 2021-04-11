84ce5a4f866e041d511412f2a373e3dc0606a5cc
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-11 22:42:09 +0530

    compare two lines using compareTO method

--- LineComparison.java
+++ LineComparison.java
@@ -1,25 +1,37 @@
+import java.lang.Math;
 import java.util.*;
+public class LineComaprison
+{
 
-public class LineComparison {
-    public static void main (String args[]) {
-        Scanner input = new Scanner(System.in);
+	public static void main(String[] args)
+	{
 
-        System.out.println("Enter the value of x1 ");
-        int x1 = input.nextInt();
+		int x1=2;
+		int x2=3;
+		int y1=2;
+		int y2=4;
 
-        System.out.println("Enter the value of y1");
-        int y1 = input.nextInt();
+		int x3=2;
+		int x4=5;
+		int y3=2;
+		int y4=4;
 
-        System.out.println("Enter the value of x2 ");
-        int x2 = input.nextInt();
+		double LengthOfLine1 = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+		System.out.println("Length of line 1 is: " +LengthOfLine1);
 
-        System.out.println("Enter the value of y2");
-        int y2 = input.nextInt();
+		double LengthOfLine2 = Math.sqrt((x4-x3)^2 + (y4-y3)^2);
+		System.out.println("Length of line 2 is: " +LengthOfLine2);
 
-        double lineLength = 0;
-        lineLength = Math.sqrt(Math.pow((x2 - x1),2)+ Math.pow((y2 - y1),2));
+		double obj1 = new Double(LengthOfLine1);
+		double obj2 = new Double(LengthOfLine2);
 
-        System.out.println("Length of line is " + lineLength);
+		int compare = obj1.compareTo(obj2);
 
-    }
+		if(compare == 1)
+			System.out.println("Line 1 is gretter than Line 2");
+		else if(compare == -1)
+			System.out.println("Line 1 is less than Line 2");
+		else if(compare == 0)
+			System.out.println("Line1 and Line2 are equals");
+	}
 }
